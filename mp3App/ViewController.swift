08b5aa3e78b0e343425a58e7b6b7ecde99d0947f
Vladimir <xalif80@gmail.com>
2023-06-05 23:41:58 +0200

    init

--- mp3App/ViewController.swift
+++ mp3App/ViewController.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var Sound: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
+        let path = Bundle.main.path(forResource: "1111.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            Sound = try AVAudioPlayer(contentsOf: url)
+            Sound?.play()
+        } catch {
+            // couldn't load file :(
+        }
         // Do any additional setup after loading the view.
     }
 
